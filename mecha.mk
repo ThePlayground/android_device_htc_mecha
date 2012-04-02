644035d369b6b8783405aa90cec71d4e6498b8be
TwistedUmbrella <twistedumbrella@gmail.com>
2012-04-02 11:42:00 -0400

    Realign build dates and device profile information

--- mecha.mk
+++ mecha.mk
@@ -230,7 +230,6 @@ $(call inherit-product, device/htc/common/common.mk)
 
 $(call inherit-product, build/target/product/full_base.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
 PRODUCT_BRAND := HTC
 PRODUCT_NAME := htc_mecha
 PRODUCT_DEVICE := mecha
