5c1cd45e9abb520e30849182904194d01dca5c75
TwistedUmbrella <twistedumbrella@gmail.com>
2012-04-19 12:52:37 -0400

    Automated Kernel Update - Mecha

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -60,9 +60,9 @@ BOARD_KERNEL_CMDLINE := no_console_suspend=1
 BOARD_KERNEL_BASE := 0x05200000
 BOARD_PAGE_SIZE := 4096
 
-# TARGET_PREBUILT_KERNEL := device/htc/mecha/kernel/kernel
-TARGET_KERNEL_CONFIG := lean_aosp_defconfig
-TARGET_KERNEL_SOURCE := kernel/htc/mecha
+TARGET_PREBUILT_KERNEL := device/htc/mecha/kernel/kernel
+# TARGET_KERNEL_CONFIG := lean_aosp_defconfig
+# TARGET_KERNEL_SOURCE := kernel/htc/mecha
 
 BOARD_HAS_LARGE_FILESYSTEM := true
 BOARD_HAS_NO_SELECT_BUTTON := true
